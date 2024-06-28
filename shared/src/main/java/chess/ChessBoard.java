8db10effbd8898927bcc93e5ce79c21beba697b0
clerkr <j.clark.roylance@gmail.com>
2024-06-27 21:29:49 -0600

    Implemented dimension checking for out-of-bounds squares, identifying if a ssquare is a piece, and determining the piece's team within the context of completing BishopMoveTests

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,8 +11,23 @@ package chess;
  */
 public class ChessBoard {
 
+    private ChessPiece[][] squares = new ChessPiece[8][8];
+
     public ChessBoard() {
         
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
     }
 
     /**
@@ -19,7 +37,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        squares[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -30,7 +48,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow() - 1][position.getColumn() - 1];
     }
 
     /**
@@ -40,4 +58,6 @@ public class ChessBoard {
     public void resetBoard() {
         throw new RuntimeException("Not implemented");
     }
+
+
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,9 +11,15 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private final ChessGame.TeamColor pieceColor;
+    private final ChessPiece.PieceType type;
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    public ChessPiece(ChessGame.TeamColor pieceColor,
+                      ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
+
 
     /**
      * The various different chess piece options
@@ -29,14 +37,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -46,7 +54,104 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
+
+    private boolean checkInBounds(int row, int col) {
+        return (row >= 1 && row <= 8) && (col >= 1 && col <= 8);
+    }
+    private boolean isPiece(ChessBoard board, int row, int col){
+        return board.getPiece(new ChessPosition(row, col)) != null;// Piece is present
+    }
+    private boolean isFriend(ChessBoard board, int row, int col){
+        ChessPosition pos = new ChessPosition(row, col);
+        ChessPiece piece = board.getPiece(pos);
+        return piece.pieceColor == pieceColor;
+
+    }
+
+    private ArrayList<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
+        ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
+        ChessMove move;
+        int row;
+        int col;
+        // Up-Right Diagonal
+        row = myPosition.getRow() + 1;
+        col = myPosition.getColumn() + 1;
+        while(true) {
+            if (!checkInBounds(row, col)) { break; }
+            if (isPiece(board, row, col) && isFriend(board, row, col)) { break; }
+            ChessPosition endPosition = new ChessPosition(row, col);
+            move = new ChessMove(myPosition, endPosition, null);
+            moves.add(move);
+            if (isPiece(board, row, col) && !isFriend(board, row, col)) { break; }
+            row += 1;
+            col += 1;
+        }
+        // Up-Left Diagonal
+        row = myPosition.getRow() + 1;
+        col = myPosition.getColumn() - 1;
+        while(true) {
+            if (!checkInBounds(row, col)) { break; }
+            if (isPiece(board, row, col) && isFriend(board, row, col)) { break; }
+            ChessPosition endPosition = new ChessPosition(row, col);
+            move = new ChessMove(myPosition, endPosition, null);
+            moves.add(move);
+            if (isPiece(board, row, col) && !isFriend(board, row, col)) { break; }
+            row += 1;
+            col -= 1;
+        }
+        // Down-Left Diagonal
+        row = myPosition.getRow() - 1;
+        col = myPosition.getColumn() - 1;
+        while(true) {
+            if (!checkInBounds(row, col)) { break; }
+            if (isPiece(board, row, col) && isFriend(board, row, col)) { break; }
+            ChessPosition endPosition = new ChessPosition(row, col);
+            move = new ChessMove(myPosition, endPosition, null);
+            moves.add(move);
+            if (isPiece(board, row, col) && !isFriend(board, row, col)) { break; }
+            row -= 1;
+            col -= 1;
+        }
+        // Down-Right Diagonal
+        row = myPosition.getRow() - 1;
+        col = myPosition.getColumn() + 1;
+        while(true) {
+            if (!checkInBounds(row, col)) { break; }
+            if (isPiece(board, row, col) && isFriend(board, row, col)) { break; }
+            ChessPosition endPosition = new ChessPosition(row, col);
+            move = new ChessMove(myPosition, endPosition, null);
+            moves.add(move);
+            if (isPiece(board, row, col) && !isFriend(board, row, col)) { break; }
+            row -= 1;
+            col += 1;
+        }
+
+        return moves;
+    }
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+
+        if (type == PieceType.BISHOP) {
+            return bishopMoves(board, myPosition);
+        }
+//        return bishopMoves(myPosition);
+        return new ArrayList<ChessMove>();
+
+
+
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 }
