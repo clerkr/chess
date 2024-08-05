2140816763321c50f4d7b9b0407c4ecd2d7602bc
clerkr <j.clark.roylance@gmail.com>
2024-08-05 14:39:04 -0600

    refactored constants to upper snake case

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final ServerFacade facade = new ServerFacade(8080);
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final ServerFacade FACADE = new ServerFacade(8080);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome to Chess 240!\nType " +
